fb5c408c7185191dc8439378de6ed92bdf17bc73
Thomas Lokshall <thomas@lokshall.com>
2014-06-20 09:40:30 +0200

    Implemented some utility functions

--- Catalog.lua
+++ Catalog.lua
@@ -34,7 +34,7 @@ end
 
 function Catalog:OnSave(level)
   if level == GameLib.CodeEnumAddonSaveLevel.Character then
-    return self.Options
+    return Catalog.Utility:TableCopyRecursive(self.Options)
   else
     return nil
   end
@@ -42,17 +42,25 @@ end
 
 function Catalog:OnRestore(level, options)
   if level == GameLib.CodeEnumAddonSaveLevel.Character then
-    self.Options = options
     for k, v in pairs(Defaults) do
-      if not self.Options[k] then
-        self.Options[k] = v
+      if not options[k] then
+        options[k] = v
       end
     end
+    self.Options = Catalog.Utility:TableCopyRecursive(options, self.Options)
   end
 end
 
 function Catalog:OnConfigure()
   --
+end
+
+function Catalog:GetLocale()
+  if self.Locale[self.Options.Locale] then
+    return self.Locale[self.Options.Locale]
+  else
+    return self.Locale["en"]
+  end
 end
 
 local CatalogInst = Catalog:new()
---
+++ Utility.lua
@@ -0,0 +1,44 @@
+Catalog.Utility = {}
+
+function Catalog.Utility:Capitalize(str)
+  return string.gsub(tostring(str), "^%1", string.upper)
+end
+
+function Catalog.Utility:TableSortPairs(tbl, key)
+  local keys = {}
+  for _, v in pairs(tbl) do
+    if v[key] ~= nil then
+      table.insert(keys, v[key])
+    end
+  end
+  table.sort(keys)
+  local i = 0
+  local iterator = function()
+    i = i + 1
+    if keys[i] ~= nil then
+      for _, v in pairs(tbl) do
+        if v[key] == keys[i] then
+          return keys[i], v
+        end
+      end
+      return nil
+    else
+      return nil
+    end
+  end
+  return iterator
+end
+
+function Catalog.Utility:TableCopyRecursive(source, destination)
+  destination = destination or {}
+  for k, v in pairs(source) do
+    if type(k) ~= "table" then
+      if type(v) ~= "table" then
+        destination[k] = v
+      else
+        destination[k] = self:TableCopyRecursive(v, destination[k])
+      end
+    end
+  end
+  return destination
+end
