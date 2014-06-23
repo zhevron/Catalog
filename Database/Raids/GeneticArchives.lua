509c2d5c521eaf0f5e64da7983b47de416540bb8
Thomas Lokshall <thomas@lokshall.com>
2014-06-23 11:27:55 +0200

    Added database for Genetic Archives

--- Database/Raids/GeneticArchives.lua
+++ Database/Raids/GeneticArchives.lua
@@ -7,5 +7,184 @@ Catalog.Database["GeneticArchives"] = {
   },
   ["type"] = "raid",
   ["bosses"] = {
+    {
+      ["name"] = {
+        ["en"] = "Genetic Monstrosity",
+        ["de"] = "Genetic Monstrosity",
+        ["fr"] = "Genetic Monstrosity"
+      },
+      ["normal"] = {
+        29603, -- Sign of Logic - Eldan
+        38769, -- Tarnished Eldan Gift
+        39510, -- Foul Bioplated Cuirass
+        42569, -- Partial Primal Pattern
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Hideously Malformed Mutant",
+        ["de"] = "Hideously Malformed Mutant",
+        ["fr"] = "Hideously Malformed Mutant"
+      },
+      ["normal"] = {
+        29603, -- Sign of Logic - Eldan
+        39529, -- Malformed Magnicite Workpants
+        42569, -- Partial Primal Pattern
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Gravitron Operator",
+        ["de"] = "Gravitron Operator",
+        ["fr"] = "Gravitron Operator"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Experiment X-89",
+        ["de"] = "Experiment X-89",
+        ["fr"] = "Experiment X-89"
+      },
+      ["normal"] = {
+        29599, -- Sign of Fire - Eldan
+        29607, -- Sign of Air - Eldan
+        38769, -- Tarnished Eldan Gift
+        39350, -- Demolitionist's Phageborn Boots
+        39474, -- Heliotropic Forcefield
+        39486, -- Necrotic Aura
+        42569, -- Partial Primal Pattern
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Kuralak the Defiler",
+        ["de"] = "Kuralak the Defiler",
+        ["fr"] = "Kuralak the Defiler"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Fetid Miscreation",
+        ["de"] = "Fetid Miscreation",
+        ["fr"] = "Fetid Miscreation"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Phagetech Guardian",
+        ["de"] = "Phagetech Guardian",
+        ["fr"] = "Phagetech Guardian"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Phage Maw",
+        ["de"] = "Phage Maw",
+        ["fr"] = "Phage Maw"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Phagetech Prototypes",
+        ["de"] = "Phagetech Prototypes",
+        ["fr"] = "Phagetech Prototypes"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Phageborn Convergence",
+        ["de"] = "Phageborn Convergence",
+        ["fr"] = "Phageborn Convergence"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Malfunctioning Gear",
+        ["de"] = "Malfunctioning Gear",
+        ["fr"] = "Malfunctioning Gear"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Malfunctioning Dynamo",
+        ["de"] = "Malfunctioning Dynamo",
+        ["fr"] = "Malfunctioning Dynamo"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Malfunctioning Piston",
+        ["de"] = "Malfunctioning Piston",
+        ["fr"] = "Malfunctioning Piston"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Malfunctioning Battery",
+        ["de"] = "Malfunctioning Battery",
+        ["fr"] = "Malfunctioning Battery"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    },
+    {
+      ["name"] = {
+        ["en"] = "Dreadphage Ohmna",
+        ["de"] = "Dreadphage Ohmna",
+        ["fr"] = "Dreadphage Ohmna"
+      },
+      ["normal"] = {
+      },
+      ["veteran"] = {
+      }
+    }
   }
 }
