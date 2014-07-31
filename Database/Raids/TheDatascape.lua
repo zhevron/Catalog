c522a3d1dd328888f68cf999d84d2f0955c20264
Thomas Lokshall <thomas@lokshall.com>
2014-07-31 13:52:18 +0200

    Added data file for The Datascape

---
+++ Database/Raids/TheDatascape.lua
@@ -0,0 +1,11 @@
+-- [Raid] The Datascape
+Catalog.Database["TheDatascape"] = {
+  ["name"] = {
+    ["fr"] = "L'Infosphère",
+    ["de"] = "Datenzone",
+    ["en"] = "The Datascape",
+  },
+  ["type"] = "raid",
+  ["bosses"] = {
+  },
+}
