1b576cc8b7cba326d3346d4546ffc5a7b25efe05
Thomas Lokshall <thomas@lokshall.com>
2014-08-18 02:15:54 +0200

    Fix for Curse package name

--- Catalog.lua
+++ Catalog.lua
@@ -3,7 +3,7 @@ local Catalog = Apollo.GetPackage("Gemini:Addon-1.1").tPackage:NewAddon("Catalog
 Catalog.Version = {
   ["Major"] = 1,
   ["Minor"] = 1,
-  ["Build"] = 1
+  ["Build"] = 0
 }
 
 Catalog.Defaults = {
